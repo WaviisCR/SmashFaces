ba5677a2e9c3d0ed1cbe36dfcbd10d16237f13ec
Waviis CR <Waviis CR>
2016-05-25 01:52:26 -0500

    pruebas jquery

--- js/main.js
+++ js/main.js
@@ -1,21 +1,65 @@
-$(document).ready (function comprobar ( {
-		
-	 };
-})
+var country = null;
+var maximo;
+var lugar = "";
+var historial = new Array();
+var aleatorio = null;
+var nomPais = "";
+var a;
+
+$(document).ready(function() {
+	selectcountry();
+	nombrePais = paisString();
+	//imagenCoder();
+});
 
 
-function clean() {
-		var textoIngresado = document.getElementById("coder-name");
-		res.value = "";
-		res.focus();
-    }
-
-function select-country() {
-		 if(select=='peru'){
-			  //peru[0].image
-			  $('#foto').attr('src','fotos/peru/'+ peru[foto].image);
-		 } else {
-			  //mexico[0].image ;
-			  $('#foto').attr('src','fotos/mexico/'+ mexico[foto].image);
-		 }
+function selectcountry() {
+	$("#country").on('change', function(){
+		country = ($("#pais").val());
+		if (country === 1){
+			lugar = mexico;
+		} else {
+			lugar = peru;
+		}
+		aleatorios();
+		imagenCoder();
+	});
 }
+
+function aleatorios() {
+	maximo = lugar.length;
+	var condicion = true;
+	do {
+		aleatorio = Math.floor(Math.random()*maximo)
+		if(historial.length == maximo){
+			aleatorio = -1
+			condicion = false;
+		} else if (historial.length == 0) {
+			historial.push(aleatorio);
+			condicion = false;
+		} else {
+			var nuevoNum = historial.indexOf(aleatorio);
+			if (nuevoNum < 0){
+				historial.push(aleatorio);
+				condicion = false;
+			}
+			else {
+				console.log(aleatorio+' repetido!')
+			}
+		}
+	} while (condicion);
+	return aleatorio;
+}
+
+function paisString() {
+	nomPais= (lugar === mexico) ? ("mexico") : ("peru");
+	return nomPais;
+}
+
+function imagenCoder() {
+	a =lugar[aleatorios()];
+	var src = "fotos/"+paisString()+"/"+a.image;
+	$("#foto").attr("src", src);
+	return a;
+}
+
