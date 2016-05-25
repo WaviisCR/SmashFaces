7857d951ac9a9147eb8808abf7fea3000ccea8c3
Waviis CR <Waviis CR>
2016-05-24 22:41:15 -0500

    construyendo jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(document).ready (function judada ( {
-	 
+$(document).ready (function comprobar ( {
+		
+	 };
 })
+
+
+function clean() {
+		var textoIngresado = document.getElementById("coder-name");
+		res.value = "";
+		res.focus();
+    }
+
+function select-country() {
+		 if(select=='peru'){
+			  //peru[0].image
+			  $('#foto').attr('src','fotos/peru/'+ peru[foto].image);
+		 } else {
+			  //mexico[0].image ;
+			  $('#foto').attr('src','fotos/mexico/'+ mexico[foto].image);
+		 }
+}
