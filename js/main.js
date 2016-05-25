9a7dae0d8ced2ec1756c3f2724884820a23148d6
WaviisCR <m.cardenas.vr@gmail.com>
2016-05-25 16:55:20 -0500

    code

--- js/main.js
+++ js/main.js
@@ -1,65 +1,79 @@
 var country = null;
 var maximo;
 var lugar = "";
-var historial = new Array();
+var historial = [];
 var aleatorio = null;
 var nomPais = "";
-var a;
 
-$(document).ready(function() {
-	selectcountry();
-	nombrePais = paisString();
-	//imagenCoder();
+//$(document).ready(function() {
+$("#country").change(function() {
+    country = $(this).val();
+    if (country === 0) {
+        lugar = peru;
+
+    } else {
+        lugar = mexico;
+    }
+    imagenCoder();
 });
-
-
-function selectcountry() {
-	$("#country").on('change', function(){
-		country = ($("#pais").val());
-		if (country === 1){
-			lugar = mexico;
-		} else {
-			lugar = peru;
-		}
-		aleatorios();
-		imagenCoder();
-	});
-}
+//});
 
 function aleatorios() {
-	maximo = lugar.length;
-	var condicion = true;
-	do {
-		aleatorio = Math.floor(Math.random()*maximo)
-		if(historial.length == maximo){
-			aleatorio = -1
-			condicion = false;
-		} else if (historial.length == 0) {
-			historial.push(aleatorio);
-			condicion = false;
-		} else {
-			var nuevoNum = historial.indexOf(aleatorio);
-			if (nuevoNum < 0){
-				historial.push(aleatorio);
-				condicion = false;
-			}
-			else {
-				console.log(aleatorio+' repetido!')
-			}
-		}
-	} while (condicion);
-	return aleatorio;
+    maximo = lugar.length;
+    var condicion = true;
+    do {
+        aleatorio = Math.floor(Math.random() * maximo)
+        if (historial.length == maximo) {
+            aleatorio = -1
+            condicion = false;
+        } else if (historial.length == 0) {
+            historial.push(aleatorio);
+            condicion = false;
+        } else {
+            var nuevoNum = historial.indexOf(aleatorio);
+            if (nuevoNum < 0) {
+                historial.push(aleatorio);
+                condicion = false;
+            } else {
+                console.log(aleatorio + ' repetido!')
+            }
+        }
+    } while (condicion);
+    return aleatorio;
 }
 
 function paisString() {
-	nomPais= (lugar === mexico) ? ("mexico") : ("peru");
-	return nomPais;
+    nomPais = (lugar === mexico) ? "mexico" : "peru";
+    return nomPais;
 }
 
 function imagenCoder() {
-	a =lugar[aleatorios()];
-	var src = "fotos/"+paisString()+"/"+a.image;
-	$("#foto").attr("src", src);
-	return a;
+    aletorio = aleatorios();
+    var sr = (country == 0) ? ("peru/" + peru[aleatorio].image) : ("mexico/" + mexico[aleatorio].image);
+    $("#fotos").html('<img class="img-responsive bandera" src="fotos/' + sr + '"/>');
 }
 
+function consultaName() {
+    var nameConsulta = "";
+    nameConsulta = (lugar == 0) ? peru[aleatorio].name : mexico[aleatorio].name;
+    console.log(nameConsulta);
+    return nameConsulta;
+}
+
+function getInputValue() {
+    var mensaje = $("#codername").val();
+    return mensaje;
+}
+
+function comprobar() {
+    if (getInputValue() === consultaName()) {
+        alert("Excelente acertaste");
+        setTimeout(imagenCoder(), 2000);
+    } else {
+        alert("Sigue Intentando");
+    }
+};
+
+$(document).ready(function() {
+    $("#comprobar").click(comprobar);
+});
